4711dd66f6818ae35b4930081e51653a8c16650d
Alex Valle <alex.valle@cit.hn>
2022-04-07 15:14:38 -0600

    dist

---
+++ dist/app/app.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayGoodBye = exports.sayHello = void 0;
+function sayHello() {
+    return 'Hello World!';
+}
+exports.sayHello = sayHello;
+function sayGoodBye() {
+    return 'Goodbye World!';
+}
+exports.sayGoodBye = sayGoodBye;
